ce86fee68e25d06dc150f0d9e4e7c2d0594568aa
Julia <yulia.bobko@mail.ru>
2023-03-16 12:54:50 +0300

    Added Header and Technologies components. 10 lessons

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+    return (<div>
+        <a href="#S">Home- </a>
+        <a href="#S">New feed- </a>
+        <a href="#S">Messages- </a>
+    </div>)
+}
---
+++ src/components/Technogies/Technologies.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Technologies() {
+    return (<div>
+        <ul>
+            <li>JS</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>React</li>
+        </ul>
+    </div>)
+}
